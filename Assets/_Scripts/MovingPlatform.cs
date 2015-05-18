69dc71ca36fc106f0ee78b729186c11d60d27826
Michael Miles <boat081062@gmail.com>
2015-05-18 16:38:50 -0500

    Moving Platforms
    Added multiple scenes for different moving platforms checking which work
    and which do not. I used both first person and third person controllers.

---
+++ Assets/_Scripts/MovingPlatform.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	// Declaration of public variables
+    public Transform platform;
+    public Transform positionOne;
+    public Transform positionTwo;
+    public float platformSpeed = 2.0f;
+
+    // Declaration of private reference variables
+    private Rigidbody refPlatformRigid;
+
+    // Declaration of private misc. variables
+    private Transform destination;
+    private Vector3 direction;
+
+    // Awake is called when the script instance is being loaded
+    public void Awake()
+    {
+        refPlatformRigid = platform.GetComponent<Rigidbody>();
+    }
+        
+    // Use this for initialization
+	void Start ()
+    {
+        SetDestination(positionOne);
+    }
+
+    // This function is called every fixed framerate frame
+    public void FixedUpdate()
+    {
+        refPlatformRigid.MovePosition(platform.position + direction * platformSpeed * Time.fixedDeltaTime);
+
+        if (Vector3.Distance(platform.position, destination.position) < platformSpeed * Time.fixedDeltaTime)
+        {
+            SetDestination(destination == positionOne ? positionTwo : positionOne);
+        }
+    }
+	
+    // Draw boxes in the area of the platform destinations
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(positionOne.position, platform.localScale);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(positionTwo.position, platform.localScale);
+    }
+
+    // Set current destination for platform movement
+    void SetDestination(Transform dest)
+    {
+        destination = dest;
+        direction = (destination.position - platform.position).normalized;
+    }
+}
---
+++ Assets/_Scripts/MovingPlatforms.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    // Declaration of public variables
+    public Transform movingPlatform;
+    public Transform positionOne;
+    public Transform positionTwo;
+    public Vector3 newDestination;
+    public string currentState;
+    public float speed;
+    public float resetTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ChangeTarget();
+	}
+
+    // This function is called every fixed framerate frame.
+    public void FixedUpdate()
+    {
+        movingPlatform.position = Vector3.Lerp(movingPlatform.position, newDestination, speed * Time.fixedDeltaTime);
+    }
+
+    void ChangeTarget()
+    {
+        if (currentState == "Moving to position 1")
+        {
+            currentState = "Moving to position 2";
+            newDestination = positionTwo.position;
+        }
+
+        else if (currentState == "Moving to position 2")
+        {
+            currentState = "Moving to position 1";
+            newDestination = positionOne.position;
+        }
+
+        else if (currentState == "")
+        {
+            currentState = "Moving to position 2";
+            newDestination = positionTwo.position;
+        }
+
+        Invoke("ChangeTarget", resetTime);
+    }
+}
