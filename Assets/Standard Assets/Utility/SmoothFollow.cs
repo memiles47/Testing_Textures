1358684b267230a5e533059d0cfc537f658b9452
Michael Miles <boat081062@gmail.com>
2015-06-02 17:51:40 -0500

    Added animations and new asset
    Added a Skeleton Asset from the asset store, applied the Click To Move
    script to this character along with a smooth follow for the camera.
    (Edited the smooth follow script to remove the camera rotation)

--- Assets/Standard Assets/Utility/SmoothFollow.cs
+++ Assets/Standard Assets/Utility/SmoothFollow.cs
@@ -31,25 +31,26 @@ namespace UnityStandardAssets.Utility
 				return;
 
 			// Calculate the current rotation angles
-			var wantedRotationAngle = target.eulerAngles.y;
+            //var wantedRotationAngle = target.eulerAngles.y;
 			var wantedHeight = target.position.y + height;
 
-			var currentRotationAngle = transform.eulerAngles.y;
+            //var currentRotationAngle = transform.eulerAngles.y;
 			var currentHeight = transform.position.y;
 
 			// Damp the rotation around the y-axis
-			currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+            //currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
 
 			// Damp the height
 			currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
 			// Convert the angle into a rotation
-			var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+            //var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
 
 			// Set the position of the camera on the x-z plane to:
 			// distance meters behind the target
 			transform.position = target.position;
-			transform.position -= currentRotation * Vector3.forward * distance;
+            //transform.position -= currentRotation * Vector3.forward * distance;
+            transform.position -= Vector3.forward * distance;
 
 			// Set the height of the camera
 			transform.position = new Vector3(transform.position.x ,currentHeight , transform.position.z);
--- Assets/_Scripts/ClickToMove.cs
+++ Assets/_Scripts/ClickToMove.cs
@@ -4,15 +4,26 @@ using System.Collections;
 public class ClickToMove : MonoBehaviour
 {
     // Declaration of public variables
+    public CharacterController charController;
     public Vector3 position;
     public float speed;
+    public AnimationClip run;
+    public AnimationClip idle;
 
+    // Declaration of private reference variables
+    private Animation anim;
 
+    // Initialize private reference variables
+    public void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
 
-	// Use this for initialization
+    // Use this for initialization
 	void Start ()
     {
-	
+        // Establish player start position
+        position = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -36,12 +47,34 @@ public class ClickToMove : MonoBehaviour
         if(Physics.Raycast(ray, out hit, 1000))
         {
             position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-            Debug.Log(position);
+            //Debug.Log(position);
         }
     }
 
     void MoveToPosition()
     {
+        if (Vector3.Distance(position, transform.position) > 0.75f)
+        {
+            // Excecuted when the gameobject is moving
+            // Calculate the location of the clicked position relative to the players location
+            Vector3 relativePos = position - transform.position;
+            Quaternion newRotation = Quaternion.LookRotation(relativePos);
 
+            // Reset x and z axis in newRotation
+            newRotation.x = 0.0f;
+            newRotation.z = 0.0f;
+
+            // Smoothly rotate the player towards the clicked position
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10.0f);
+
+            charController.Move(transform.forward * Time.deltaTime * speed);
+
+            anim.CrossFade(run.name);
+        }
+        else
+        {
+            // Excuted when the gameobject is not moving
+            anim.CrossFade(idle.name);
+        }
     }
 }
