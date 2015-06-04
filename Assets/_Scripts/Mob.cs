4d4cb0245ba0d860cd2b66a60ad9ee2c30a148d2
Michael Miles <boat081062@gmail.com>
2015-06-04 17:01:59 -0500

    Completed movement for enemy and player
    See script comments for changes I made to the scripting.

--- Assets/_Scripts/Mob.cs
+++ Assets/_Scripts/Mob.cs
@@ -6,32 +6,48 @@ public class Mob : MonoBehaviour
     // Declaration of public variables
     public float speed;
     public float range;
-    public AnimationClip idle;
     public AnimationClip run;
+    public AnimationClip idle;
 
     // Declaration of private reference variables
     private Transform player;
     private Animation anim;
-    private CharacterController enemyController;
+    private CharacterController charController;
+    private PlayerCombat playerCombat;
 
     // Awake is called when the script instance is being loaded
-    // Everything in the Awake function is my variation to the code
+    // Everything in the Awake function is my variation to the code and
+    // initiates the private variables rather than the drag and drop in
+    // in the inspector
     public void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        enemyController = GetComponent<CharacterController>();
         anim = GetComponent<Animation>();
+        charController = GetComponent<CharacterController>();
+        playerCombat = player.GetComponent<PlayerCombat>();
     }
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Chase();
+        // Check if player is in range, if so do not run, you will be attacking,
+        // if not, chase at speed
+        if (!InRange())
+        {
+            // Not in range, chase player character
+            Chase();
+        }
+        else
+        {
+            // In rangem, stop and attack
+            anim.CrossFade(idle.name);
+        }
     }
 
     bool InRange()
@@ -48,6 +64,21 @@ public class Mob : MonoBehaviour
 
     void Chase()
     {
+        // Rotate to look at player
         transform.LookAt(player.position);
+
+        // Chase Player Character
+        // Note: I am using Move here rather than simplemove because if you are
+        // going to do extra with your game object (jump, fight etc, it is best
+        // to use. (See Unity Answers for the difference)
+        charController.Move(transform.forward * Time.deltaTime * speed);
+        anim.CrossFade(run.name);
+    }
+
+    // OnMouseOver is called every frame while the mouse is over the GUIElement or Collider
+    public void OnMouseOver()
+    {
+        Debug.Log("Mouse is over Mob");
+        playerCombat.opponent = gameObject;
     }
 }
---
+++ Assets/_Scripts/PlayerCombat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCombat : MonoBehaviour
+{
+    // Declaration of public reference variables
+    public GameObject opponent; // This variable must be pubic because it will be accessed outsied this script
+
+    // Initialize private reference variables
+    void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(opponent);
+	}
+}
